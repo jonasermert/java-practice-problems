765b42ab50d6e7deb09c75a519c1a1cc4907d092
ermjonertas <ermert.jonas@yahoo.de>
2023-08-28 08:46:41 +0200

    Implemented Accumulate.java

--- src/main/java/Accumulate.java
+++ src/main/java/Accumulate.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.UnaryOperator;
 
 public class Accumulate {
 
     public static <T> List<T> accumulate(List<T> list, UnaryOperator<T> operator) {
-        throw new UnsupportedOperationException("Please implement the Accumulate.accumulate() method.");
+        List<T> result = new ArrayList<>();
+        for (T element : list) {
+            result.add(operator.apply(element));
+        }
+        return result;
     }
 
+    public static void main(String[] args) {
+        List<Integer> input = List.of(1, 2, 3, 4, 5);
+        UnaryOperator<Integer> square = x -> x * x;
+        List<Integer> squaredValues = accumulate(input, square);
+        System.out.println(squaredValues);
+    }
 }
